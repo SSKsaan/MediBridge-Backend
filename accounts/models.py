2be9d92e4c6b3f4cb820dfcc015987f1327bd2e9
SaminSadik <Saminsadikkhan@gmail.com>
2025-07-26 19:55:07 +0600

    User model initialized

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    contact = models.CharField(max_length=20, unique=True)
+    birth_date = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=[
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other')
+    ], null=True, blank=True)
+    balance = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.username
