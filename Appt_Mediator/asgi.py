6de6ae7ca64902a3a0b324b494b502425604f41d
SaminSadik <Saminsadikkhan@gmail.com>
2025-07-26 11:54:16 +0600

    Project initialized

---
+++ Appt_Mediator/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Appt_Mediator project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Appt_Mediator.settings')
+
+application = get_asgi_application()
---
+++ Appt_Mediator/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Appt_Mediator project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Appt_Mediator.settings')
+
+application = get_wsgi_application()
