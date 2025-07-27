719e5f59e6628b9e382770bc7626626707adeeb2
SaminSadik <Saminsadikkhan@gmail.com>
2025-07-27 11:16:54 +0600

    Transaction model initialized

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Transaction(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='transactions')
+    amount = models.PositiveIntegerField()
+    transaction_type = models.CharField(max_length=10, choices=[
+        ('recharge', 'Recharge'),
+        ('deduct', 'Deduct'),
+        ('bonus', 'Signup Bonus')
+    ])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"[{self.transaction_type}] {self.user.username} : {self.amount}"
