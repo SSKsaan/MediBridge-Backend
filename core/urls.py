15d7b675b268d9dea6cc1cddfbdcc2b6d45345c1
SaminSadik <Saminsadikkhan@gmail.com>
2025-07-27 11:32:48 +0600

    api test

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test', views.test_api, name='test'),
+    path('test-f/', views.test_api, name='test-f'),
+    path('test-c/', views.TestAPI.as_view(), name='test-c'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
 @api_view(['GET'])
 def test_api(request):
@@ -9,3 +11,7 @@ def test_api(request):
         "message": "API is working!",
         "status": "ok"
     })
+
+class TestAPI(APIView):
+    def get(self, request):
+        return Response({"message": "DRF is working!"}, status=status.HTTP_200_OK)
