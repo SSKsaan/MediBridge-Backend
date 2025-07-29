ea4cacfe4db3790366ef0bfa1958645732d41d8f
SaminSadik <Saminsadikkhan@gmail.com>
2025-07-29 23:53:52 +0600

    Replaced username with email in the User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is Required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        if not password:
+            raise ValueError('Password is Required')
+        return self.create_user(email, password, **extra_fields)
+
 class User(AbstractUser):
+    objects = UserManager()
+    username = None
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    full_name = models.CharField(max_length=100)
     contact = models.CharField(max_length=20, unique=True)
     birth_date = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=10, choices=[
@@ -14,4 +39,4 @@ class User(AbstractUser):
     balance = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return self.username
+        return self.full_name
